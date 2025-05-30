8dbef26bb0bb3c5690332eb78e265de05b930966
dava12321 <55383271+dava12321@users.noreply.github.com>
2025-05-31 03:16:19 +0700

    new bang-desinta

--- models/prioritas.py
+++ models/prioritas.py
@@ -1,6 +1,11 @@
 from extensions import db
 
 class Prioritas(db.Model):
+    __tablename__ = 'prioritas'
+
     id = db.Column(db.Integer, primary_key=True)
     kategori = db.Column(db.String(100), nullable=False)
     persentase = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Prioritas {self.kategori} - {self.persentase}%>"
